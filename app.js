358e7402edac24399b064f9e68a0e939cb29c900
Harshit-Maurya <44harshitmaurya@gmail.com>
2024-07-04 22:16:27 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const playerSign = document.querySelector(".player");
-const computerSign = document.querySelector(".computer")
-const gameComment = document.querySelector(".game_comment")
-const buttons = document.querySelectorAll(".signs")
+const computerSign = document.querySelector(".computer");
+const gameComment = document.querySelector(".game_comment");
+const buttons = document.querySelectorAll(".signs");
 
+// Creating Variables for storing sign of attaking and defending in game 
 let playerS= "" , computerS= "";
+
+// onclicking icons of signs that play the game
 const playerTurn = (turn) =>{
     if(turn == "stone"){
         playerSign.src = "img/Player_stone.png";
     }else if(turn == "paper"){
-        playerSign.src = "img/player_paper.png"
+        playerSign.src = "img/player_paper.png";
     }else if(turn == "scissor"){
-        playerSign.src = "img/Player_scissor.png"
+        playerSign.src = "img/Player_scissor.png";
     }
     playerS = turn;
     setTimeout(()=>{
@@ -19,33 +22,35 @@ const playerTurn = (turn) =>{
     },500);
 }
 
+// random computer turn 
 const computerTurn= () =>{
     let a = "";
     let randomSign = (Math.floor(Math.random() * 3)) + 1;
     if(randomSign === 1){
-        computerSign.src = "img/computer_stone.png"
+        computerSign.src = "img/computer_stone.png";
         a = "stone";
     }else if(randomSign === 2){
-        computerSign.src = "img/computer_paper.png"
+        computerSign.src = "img/computer_paper.png";
         a = "paper";
     }else if(randomSign === 3){
-        computerSign.src = "img/computer_scissor.png"
+        computerSign.src = "img/computer_scissor.png";
         a = "scissor";
     }
-    console.log(randomSign)
+    console.log(randomSign);
     computerS = a;
 }
 
+// checking the winner 
 const checkWinner = (computer,player) =>{
     if((computer == "stone" && player == "scissor") || (computer == "scissor" && player == "paper") || (computer == "paper" && player == "stone")){
         disableAllBtn();
-        gameComment.innerText = "Ohh You Lose ! restart for next game."
+        gameComment.innerText = "Ohh You Lose ! restart for next game.";
     }else if(computer == player){
         disableAllBtn();
-        gameComment.innerText = "Ohh Draw ! restart for next game."
+        gameComment.innerText = "Ohh Draw ! restart for next game.";
     }else{
         disableAllBtn();
-        gameComment.innerText = "Hurray You Won ! restart for next game."
+        gameComment.innerText = "Hurray You Won ! restart for next game.";
     }
 }
 
